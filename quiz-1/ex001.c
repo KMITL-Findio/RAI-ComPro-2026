639e82256786b8773a539bd4d4389bd923defb62
KMITL-Findio <69011483@kmitl.ac.th>
2026-07-17 06:29:44 +0000

    Quiz1_lab-2

--- quiz-1/ex001.c
+++ quiz-1/ex001.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-    printf("%-16s : %s\n", "Programming", "A");
-    printf("%-16s : %s\n", "Drawing", "B+");
-    printf("%-16s : %s\n", "Mechanics", "B+");
-    printf("%-16s : %s\n", "Math", "B");
-    printf("%-16s : %.2f\n", "GPS", 3.5);
+    printf("%s : %s\n", "Programming", "A");
+    printf("%s     : %s\n", "Drawing", "B+");
+    printf("%s   : %s\n", "Mechanics", "B+");
+    printf("%s        : %s\n", "Math", "B");
+    printf("%s         : %.2f\n", "GPS", 3.5);
 
     return 0;
 }
